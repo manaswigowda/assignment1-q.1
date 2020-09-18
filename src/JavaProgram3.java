29df86664e3e707cfd15dfd6d38bbf99a1bf4268
manaswigowda <manaswi2606gowda@gmail.com>
2020-09-18 15:16:46 +0530

    Java Program 3

---
+++ src/JavaProgram3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class JavaProgram3 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
